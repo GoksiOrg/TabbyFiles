0d10accc40a75d8851a8a92bd2ad09d29a8008a7
Vukašin Lekić <goksi1312@gmail.com>
2023-08-19 18:18:05 +0200

    Prefixing all classes annotated with @RestController with /api/

---
+++ src/main/kotlin/tech/goksi/tabbyfiles/configuration/WebMvcConfiguration.kt
@@ -0,0 +1,15 @@
+package tech.goksi.tabbyfiles.configuration
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.method.HandlerTypePredicate
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebMvcConfiguration : WebMvcConfigurer {
+
+    override fun configurePathMatch(configurer: PathMatchConfigurer) {
+        configurer.addPathPrefix("/api", HandlerTypePredicate.forAnnotation(RestController::class.java))
+    }
+}
