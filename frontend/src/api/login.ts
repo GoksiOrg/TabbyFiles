da40caf403e5c2248a40f12ef43af2f818c231ec
Vukašin Lekić <goksi1312@gmail.com>
2023-08-22 18:20:04 +0200

    Update login.ts

--- frontend/src/api/login.ts
+++ frontend/src/api/login.ts
@@ -9,10 +9,12 @@ export default function login(data: LoginData) {
     return new Promise((resolve, reject) => {
         http.post('/api/csrf')
             .then(() => {
-                http.post('/auth/login', data).then(response => {
-                    window.location = response.request.responseURL;
-                    resolve(response);
-                });
+                http.post('/auth/login', data)
+                    .then(response => {
+                        window.location = response.request.responseURL;
+                        resolve(response);
+                    })
+                    .catch(reject);
             })
             .catch(reject);
     });
