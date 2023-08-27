1f41ccc554f16a237d1bf77f5d43f7b4e873aa07
Vukašin Lekić <goksi1312@gmail.com>
2023-08-27 15:04:52 +0200

    Update TabbyFilesApplication.kt

--- src/main/kotlin/tech/goksi/tabbyfiles/TabbyFilesApplication.kt
+++ src/main/kotlin/tech/goksi/tabbyfiles/TabbyFilesApplication.kt
@@ -1,21 +1,28 @@
 package tech.goksi.tabbyfiles
 
-import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import tech.goksi.tabbyfiles.cli.CommandInvoker
+import tech.goksi.tabbyfiles.utils.ResourceUtils
+import tech.goksi.tabbyfiles.utils.SLF4J
 import java.io.File
+import java.util.*
 
 @SpringBootApplication
 class TabbyFilesApplication
 
 fun main(args: Array<String>) {
+    if (args.isNotEmpty() && args[0].equals("cli", true)) {
+        val commandInvoker = CommandInvoker()
+        val cliArgs = LinkedList(args.asList())
+        commandInvoker.invoke(cliArgs)
+        return
+    }
     val configurationFile = File("application.yml")
     if (!configurationFile.exists()) {
-        val logger = LoggerFactory.getLogger(TabbyFilesApplication::class.java)
+        val logger by SLF4J
         logger.info("Looks like you don't have configuration YAML file, generating one...")
-        val configurationContent = TabbyFilesApplication::class.java.classLoader
-            .getResource("default-application.yml")!!.readText()
-        configurationFile.writeText(configurationContent)
+        ResourceUtils.writeResource(configurationFile, "default-application.yml")
         logger.info("Configuration file generated, edit it to your desires and start application again !")
         return
     }
