0428b1ffe5c16c27d4536e8d3adf33ccb5186a28
Vukašin Lekić <goksi1312@gmail.com>
2023-09-29 17:31:55 +0200

    Fixing issue where delegate would not work if no class is present

--- src/main/kotlin/tech/goksi/tabbyfiles/utils/SLF4J.kt
+++ src/main/kotlin/tech/goksi/tabbyfiles/utils/SLF4J.kt
@@ -2,11 +2,13 @@ package tech.goksi.tabbyfiles.utils
 
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import tech.goksi.tabbyfiles.TabbyFilesApplication
 import kotlin.reflect.KProperty
 
 object SLF4J {
     operator fun getValue(refClass: Any?, prop: KProperty<*>): Logger {
-        return LoggerFactory.getLogger(refClass!!::class.java)
+        return if (refClass == null) LoggerFactory.getLogger(TabbyFilesApplication::class.java)
+        else LoggerFactory.getLogger(refClass::class.java)
     }
 
     operator fun invoke(name: String) = lazy {
