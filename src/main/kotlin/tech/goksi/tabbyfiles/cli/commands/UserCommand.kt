5ac4293d9f06bfb7d29e9a54b4dc075ab8b3299a
Vukašin Lekić <goksi1312@gmail.com>
2023-10-04 04:04:37 +0200

    Finishing all user commands for now

--- src/main/kotlin/tech/goksi/tabbyfiles/cli/commands/UserCommand.kt
+++ src/main/kotlin/tech/goksi/tabbyfiles/cli/commands/UserCommand.kt
@@ -1,15 +1,15 @@
 package tech.goksi.tabbyfiles.cli.commands
 
 import jakarta.validation.Validator
-import org.jline.terminal.Terminal
+import org.springframework.security.core.userdetails.UsernameNotFoundException
+import org.springframework.shell.component.ConfirmationInput
+import org.springframework.shell.component.ConfirmationInput.ConfirmationInputContext
 import org.springframework.shell.component.flow.ComponentFlow
 import org.springframework.shell.component.flow.SelectItem
-import org.springframework.shell.standard.ShellCommandGroup
-import org.springframework.shell.standard.ShellComponent
-import org.springframework.shell.standard.ShellMethod
-import org.springframework.shell.table.BeanListTableModel
-import org.springframework.shell.table.BorderStyle
-import org.springframework.shell.table.TableBuilder
+import org.springframework.shell.standard.*
+import org.springframework.web.server.ResponseStatusException
+import tech.goksi.tabbyfiles.cli.services.ConsoleServices
+import tech.goksi.tabbyfiles.cli.services.TableServices
 import tech.goksi.tabbyfiles.requests.UserRequest
 import tech.goksi.tabbyfiles.services.RoleService
 import tech.goksi.tabbyfiles.services.TabbyUserService
@@ -19,24 +19,21 @@ import tech.goksi.tabbyfiles.services.TabbyUserService
 class UserCommand(
     private val userService: TabbyUserService,
     private val roleService: RoleService,
-    private val terminal: Terminal,
+    private val console: ConsoleServices,
     private val validator: Validator,
-    private val flowBuilder: ComponentFlow.Builder
-) {
+    private val flowBuilder: ComponentFlow.Builder,
+    private val tableServices: TableServices
+) : AbstractShellComponent() {
     @ShellMethod(key = ["user-list"], value = "Shows list of all currently registered users")
     fun list() {
         val users = userService.getAllUsers()
-        val tableModel =
-            BeanListTableModel(
-                users,
-                linkedMapOf("id" to "ID", "username" to "Username", "roles" to "Roles", "createdAt" to "Registered at")
-            )
-        val tableBuilder = TableBuilder(tableModel)
-        tableBuilder.addFullBorder(BorderStyle.fancy_light)
-        terminal.writer().println(tableBuilder.build().render(80))
+        tableServices.printTable(
+            users,
+            linkedMapOf("id" to "ID", "username" to "Username", "roles" to "Roles", "createdAt" to "Registered at")
+        )
     }
 
-    @ShellMethod(key = ["user-register"], value = "Register new user on the system")
+    @ShellMethod(key = ["user-create"], value = "Register new user on the system")
     fun register() {
         val roles = roleService.getAllRoles()
         val rolesToSelect = roles.map { SelectItem.of(it.name, it.name) }
@@ -65,15 +62,58 @@ class UserCommand(
         if (failed.isEmpty()) {
             try {
                 val user = userService.addUser(userRequest)
-                terminal.writer()
-                    .println("New user with username \"$username\" and id ${user.id} is successfully added to the system !")
+                console.success(
+                    "New user with username %s and id %d is successfully added to the system !",
+                    username,
+                    user.id
+                )
+
             } catch (exception: Exception) {
-                terminal.writer().println("Error while adding new user !") // TODO
+                console.error("Error while adding new user !") // TODO
+                throw exception
             }
         } else {
             for (constraintViolation in failed) {
-                terminal.writer().println(constraintViolation.message)
+                console.error(constraintViolation.message)
             }
+        }
+    }
+
+    @ShellMethod(key = ["user-info"], value = "Display specific user info")
+    fun info(@ShellOption(help = "Username of the user", arity = 1) username: String) {
+        val user = try {
+            userService.getUserByUsername(username)
+        } catch (exception: UsernameNotFoundException) {
+            console.error("User with username %s is not found in the system !", username)
+            null
+        } ?: return
+        tableServices.printTable(
+            user,
+            linkedMapOf("id" to "ID", "username" to "Username", "roles" to "Roles", "createdAt" to "Registered at")
+        )
+    }
+
+    @ShellMethod(key = ["user-delete"], value = "Delete specific user from the system")
+    fun delete(@ShellOption(help = "ID of user u wish to remove from system") id: Long) {
+        val user = try {
+            userService.getUserById(id)
+        } catch (exception: ResponseStatusException) {
+            if (exception.statusCode.value() == 404) {
+                console.error("User with id %d is not found in the system !", id)
+                null
+            } else throw exception
+        } ?: return
+        val confirmation =
+            ConfirmationInput(terminal, "Are you sure that you want to delete user ${user.username} ?", false)
+        confirmation.setResourceLoader(resourceLoader)
+        confirmation.templateExecutor = templateExecutor
+        val resultContext = confirmation.run(ConfirmationInputContext.empty())
+
+        if (resultContext.resultValue) {
+            userService.deleteUser(user)
+            console.success("User with username %s is successfully deleted from the system !", user.username)
+        } else {
+            console.write("Aborting...")
         }
     }
 
