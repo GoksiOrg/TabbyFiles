2cecfe36985fee276c38575190a36a19a18603f0
Vukašin Lekić <goksi1312@gmail.com>
2023-10-11 02:30:17 +0200

    Refactor

--- src/main/kotlin/tech/goksi/tabbyfiles/utils/version/http/client/GithubClient.kt
+++ src/main/kotlin/tech/goksi/tabbyfiles/utils/version/http/client/GithubClient.kt
@@ -1,10 +1,10 @@
 package tech.goksi.tabbyfiles.utils.version.http.client
 
 import org.springframework.web.service.annotation.GetExchange
-import tech.goksi.tabbyfiles.utils.version.http.response.GithubResponse
+import tech.goksi.tabbyfiles.utils.version.http.response.GithubReleaseResponse
 
 interface GithubClient {
 
     @GetExchange("/releases/latest")
-    fun getLatestRelease(): GithubResponse
+    fun getLatestRelease(): GithubReleaseResponse
 }
--- src/main/kotlin/tech/goksi/tabbyfiles/utils/version/http/response/GithubReleaseResponse.kt
+++ src/main/kotlin/tech/goksi/tabbyfiles/utils/version/http/response/GithubReleaseResponse.kt
@@ -3,6 +3,6 @@ package tech.goksi.tabbyfiles.utils.version.http.response
 import com.fasterxml.jackson.annotation.JsonProperty
 import tech.goksi.tabbyfiles.utils.version.Version
 
-data class GithubResponse(@JsonProperty("tag_name") private val tagName: String) {
+data class GithubReleaseResponse(@JsonProperty("tag_name") private val tagName: String) {
     val version: Version by lazy { Version(tagName) }
 }
