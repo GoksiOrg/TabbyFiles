9e25865212b67727297c4800e3181fb47b665d8b
Vukašin Lekić <goksi1312@gmail.com>
2023-10-21 16:56:17 +0200

    Made sass variables work

--- frontend/src/components/NotFound.tsx
+++ frontend/src/components/NotFound.tsx
@@ -1,18 +1,16 @@
 import { useNavigate } from 'react-router-dom';
-/*TODO: variables not working*/
+
 export default function NotFound() {
     const navigate = useNavigate();
     return (
-        <div className='d-flex justify-content-center align-items-center flex-column vh-100'>
+        <div className='flex-center flex-column vh-100'>
             <div className='position-relative'>
                 <img src='/static/error_cat.png' alt='Error cat' style={{ maxWidth: '100%', height: 'auto' }} />
-                <p className='display-4 position-absolute bottom-0 translate-middle' style={{ left: '83%' }}>
-                    404
-                </p>
+                <p className='display-4 position-absolute bottom-0 translate-middle start-83'>404</p>
             </div>
             <p className='h4 mb-4'>Requested resource can't be found !</p>
             <button
-                className='btn btn-primary btn-block'
+                className='btn btn-primary'
                 onClick={() => {
                     navigate('/');
                 }}
