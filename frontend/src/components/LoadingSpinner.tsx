da29ed6d0cf3475986bd502625ba3c2c2f3eac42
Vukašin Lekić <goksi1312@gmail.com>
2023-10-22 02:42:12 +0200

    Update LoadingSpinner.tsx

--- frontend/src/components/LoadingSpinner.tsx
+++ frontend/src/components/LoadingSpinner.tsx
@@ -1,9 +1,6 @@
 export default function LoadingSpinner() {
     return (
-        <div
-            className='d-flex justify-content-center align-items-center spinner-border text-primary'
-            role='status'
-        >
+        <div className='flex-center spinner-border text-primary' role='status'>
             <span className='sr-only'>Loading...</span>
         </div>
     );
