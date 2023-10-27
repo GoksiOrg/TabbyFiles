531215bdc85e94b9b3e22767606342165bca5f80
Vukašin Lekić <goksi1312@gmail.com>
2023-10-28 01:51:34 +0200

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,38 +7,42 @@ import MainRouter from './routers/MainRouter.tsx';
 import { ErrorBoundary } from 'react-error-boundary';
 import GlobalError from './components/GlobalError.tsx';
 import NotFound from './components/NotFound.tsx';
+import { Provider } from 'react-redux';
+import store from './state/store.ts';
 
 export default function App() {
     return (
-        <div className='mx-auto w-auto'>
-            <BrowserRouter>
-                <ErrorBoundary
-                    fallbackRender={fallbackProps => <GlobalError fallbackProps={fallbackProps} />}
-                    onError={console.log}
-                    onReset={() => window.location.reload()}
-                >
-                    <Routes>
-                        <Route
-                            path={'/'}
-                            element={
-                                <Suspense fallback={<LoadingSpinner />}>
-                                    <MainRouter />
-                                </Suspense>
-                            }
-                        />
-                        <Route
-                            path={'/auth/login'}
-                            element={
-                                <Suspense fallback={<LoadingSpinner />}>
-                                    <LoginPage />
-                                </Suspense>
-                            }
-                        />
-                        <Route path={'*'} element={<NotFound />} />
-                    </Routes>
-                </ErrorBoundary>
-            </BrowserRouter>
-            <TabbyFooter />
-        </div>
+        <Provider store={store}>
+            <div className='mx-auto w-auto'>
+                <BrowserRouter>
+                    <ErrorBoundary
+                        fallbackRender={fallbackProps => <GlobalError fallbackProps={fallbackProps} />}
+                        onError={console.log}
+                        onReset={() => window.location.reload()}
+                    >
+                        <Routes>
+                            <Route
+                                path={'/'}
+                                element={
+                                    <Suspense fallback={<LoadingSpinner />}>
+                                        <MainRouter />
+                                    </Suspense>
+                                }
+                            />
+                            <Route
+                                path={'/auth/login'}
+                                element={
+                                    <Suspense fallback={<LoadingSpinner />}>
+                                        <LoginPage />
+                                    </Suspense>
+                                }
+                            />
+                            <Route path={'*'} element={<NotFound />} />
+                        </Routes>
+                    </ErrorBoundary>
+                </BrowserRouter>
+                <TabbyFooter />
+            </div>
+        </Provider>
     );
 }
