79c9d1143caa0990b8d1018d742fa3424fbf2b9c
Vukašin Lekić <goksi1312@gmail.com>
2023-11-09 02:24:34 +0100

    Make our filter call Auth success handler

--- src/main/kotlin/tech/goksi/tabbyfiles/filters/JsonAuthenticationFilter.kt
+++ src/main/kotlin/tech/goksi/tabbyfiles/filters/JsonAuthenticationFilter.kt
@@ -9,11 +9,15 @@ import org.springframework.http.HttpMethod
 import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.Authentication
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher
 
-class JsonAuthenticationFilter(private val objectMapper: ObjectMapper, authManager: AuthenticationManager) :
-    UsernamePasswordAuthenticationFilter(authManager) {
+class JsonAuthenticationFilter(
+    private val objectMapper: ObjectMapper,
+    authManager: AuthenticationManager,
+    private val successHandler: AuthenticationSuccessHandler
+) : UsernamePasswordAuthenticationFilter(authManager) {
     init {
         setRequiresAuthenticationRequestMatcher(AntPathRequestMatcher.antMatcher(HttpMethod.POST, "/auth/login"))
     }
@@ -26,7 +30,9 @@ class JsonAuthenticationFilter(private val objectMapper: ObjectMapper, authManag
         request.inputStream.use {
             val authRequest = objectMapper.readValue<AuthenticationRequest>(it)
             val token = UsernamePasswordAuthenticationToken(authRequest.username, authRequest.password)
-            return this.authenticationManager.authenticate(token);
+            val authentication = this.authenticationManager.authenticate(token)
+            successHandler.onAuthenticationSuccess(request, response, authentication)
+            return authentication
         }
     }
 }
